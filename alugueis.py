b799d7bd7644a681711397adf5b993f944f6d162
LeoMorettoo <leo152010@live.com>
2019-06-17 17:56:14 -0300

    Add files via upload

--- alugueis.py
+++ alugueis.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import json
 from bs4 import BeautifulSoup
 from unicodedata import normalize
 
@@ -15,13 +16,19 @@ def getSinopse(sinopse):
 	pass
 	return descricao_attributos
 
-def getItensImovel(itens):
-	teste = []
+def getItensDescricao(itens):
+	retorno = []
 	for y in itens.find_all('li'):
-		descricao_piece = normalizeText(y.getText().strip().lower())
-		teste.append(descricao_piece)
+		retorno.append(normalizeText(y.getText().strip().lower()))
 	pass
-	return teste
+	return retorno
+
+def getValores(blocks):
+	retorno = {}
+	for block in blocks:
+		retorno[normalizeText(block.find('small').getText()).strip().lower().replace(':','')] = block.find('span').getText().strip()
+		pass
+	return retorno
 
 
 
@@ -35,23 +42,30 @@ if req.status_code == 200:
 	for x in alugueis:
 		links.append(x.find('a')['href'])
 		pass
-
+	alugueis = []
 	for link in links:
 		req = requests.get(base_url + link)
 		if req.status_code == 200:
+			aluguel_object = {}
 			soup = BeautifulSoup(req.content, 'html.parser')
-			geo = soup.find('meta',{"name":"ICBM"})['content']
-			valores = soup.find_all('div', class_='price-block')
-			preco = valores[0].find('span').find('div').find_next_sibling().getText()
-			condominio = valores[1].find('span').getText()
-			iptu = valores[2].find('span').getText()
-
+			aluguel_object['geo'] = soup.find('meta',{"name":"ICBM"})['content']
+			aluguel_object['valores'] = getValores(soup.find_all('div', class_='price-block'))
+			# preco = valores[0].find('span').find('div').find_next_sibling().getText()
+			# condominio = valores[1].find('span').getText()
+			# iptu = valores[2].find('span').getText()
 			descricoes = soup.find_all('ul', class_='property-amenities-list')
 			descricoes_itens = {}
-			sinopse_temp1 = getSinopse(descricoes[0])
-			sinopse_temp2 = getSinopse(descricoes[1])
-			descricoes_itens['sinopse'] = dict(**sinopse_temp1,**sinopse_temp2)
-			descricoes_itens['itens_do_imovel'] = getItensImovel(descricoes[2])
-			print(descricoes_itens)
-			exit()
+			try:
+				sinopse_temp1 = getSinopse(descricoes[0])
+				sinopse_temp2 = getSinopse(descricoes[1])
+				descricoes_itens['sinopse'] = dict(**sinopse_temp1,**sinopse_temp2)
+				descricoes_itens['itens_do_imovel'] = getItensDescricao(descricoes[2])
+				descricoes_itens['itens_do_edificio'] = getItensDescricao(descricoes[3])
+				pass
+			except Exception as e:
+				pass
+			aluguel_object['descricoes'] = descricoes_itens
+			alugueis.append(aluguel_object)
 		pass
+
+print(json.dumps(alugueis,indent=4))
