1b302ae615d24cdf92022a99f1a601a4b093114f
LeoMorettoo <leo152010@live.com>
2019-07-12 17:46:44 -0300

    Add files via upload

--- alugueisGeral.py
+++ alugueisGeral.py
@@ -4,25 +4,19 @@ from user_agent import generate_user_agent
 import re
 
 def achou(item):
-	return item != []
+	return item is not None
 
 def acharNumeroDeBanheiros(soup,item_para_achar):
 	itens_achados = soup.find_all(text=re.compile(item_para_achar))
-	patterns = ['/((^([Bb])anheiros?)\s*)([1-9]*)/g','/((([1-9]*))\s*(([Bb])anheiros?))/g']
+	patterns = ['((^([Bb])anheiros?)\s*)([1-9]*)','(([1-9]*))\s*(([Bb])anheiros?)']
 	for banheiro in itens_achados:
 		for pattern in patterns:
-			print(banheiro)
-			print(re.search('Banheiros', banheiro))
-			if achou(banheiro):
-				print('achou')
-				pass
-			else:
-				print('nao achou')
+			numero_de_banheiros = re.search(pattern, banheiro)
+			if achou(numero_de_banheiros):
+				print(numero_de_banheiros.groups())
 				pass
 			pass
-		#print(banheiros)
 	exit()
-	#return soup.find_all(text=re.compile('Banheiros')
 
 url = 'https://www.gpsimoveis.imb.br/imovel/casa-com-4-quartos-para-locacao-r-4-500-00-jardim-portal-dos-ipes-indaiatuba-sp/CA10386-CS0'
 headers = {'User-Agent': generate_user_agent(device_type="desktop", os=('mac', 'linux'))}
