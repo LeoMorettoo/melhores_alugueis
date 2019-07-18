e71489ca46577b4ebb6d4f403d7209ef6fb1e155
Leonardo <leo@brochura.local>
2019-07-18 17:56:01 -0300

    pequenas mudanças

--- alugueisGeral.py
+++ alugueisGeral.py
@@ -13,7 +13,10 @@ def onlyNumbers(val):
 def getItenWithREGEX(itens_achados,patterns):
 	for iten in itens_achados:
 		for pattern in patterns:
-			numero_de_itens = re.search(pattern, iten)
+			try:
+				numero_de_itens = re.search(pattern, iten)
+			except Exception as e:
+				return None
 			if itemfound(numero_de_itens):
 				return numero_de_itens.group()
 	return None
@@ -43,19 +46,19 @@ def getItem(soup,item_para_achar,patterns,apenasNUmeros = True):
 	return None
 
 def getBanheiros(soup,item_para_achar):
-	patterns = ['(\s?)(:?)(\s?)(([0-9]*)?)(\s?)(' + item_para_achar + '\s*)(\s?)(:?)(\s?)(([0-9]*)?)(\s?)']
+	patterns = ['(\s?)(:?)(\s?)([0-9]+)(\s?)(' + item_para_achar + '\s*)|(' + item_para_achar + '\s*)(\s?)(:?)(\s?)([0-9]+)(\s?)']
 	return getItem(soup,item_para_achar,patterns)
 
 def getQuartos(soup,item_para_achar):
-	patterns = ['(\s?)(:?)(\s?)(([0-9]*)?)(\s?)(' + item_para_achar + '\s*)(\s?)(:?)(\s?)(([0-9]*)?)(\s?)']
+	patterns = ['(\s?)(:?)(\s?)([0-9]+)(\s?)(' + item_para_achar + '\s*)|(' + item_para_achar + '\s*)(\s?)(:?)(\s?)([0-9]+)(\s?)']
 	return getItem(soup,item_para_achar,patterns)
 
 def getSuites(soup,item_para_achar):
-	patterns = ['(\s?)(:?)(\s?)(([0-9]*)?)(\s?)(' + item_para_achar + '\s*)(\s?)(:?)(\s?)(([0-9]*)?)(\s?)']
+	patterns = ['(\s?)(:?)(\s?)([0-9]+)(\s?)(' + item_para_achar + '\s*)|(' + item_para_achar + '\s*)(\s?)(:?)(\s?)([0-9]+)(\s?)']
 	return getItem(soup,item_para_achar,patterns)
 
 def acharCondominio(soup,item_para_achar):
-	patterns = ['(\s?)(:?)(\s?)(([0-9]*)?)(\s?)(' + item_para_achar + '\s*)(\s?)(:?)(\s?)(([0-9]*)?)(\s?)']
+	patterns = ['(\s?)(:?)(\s?)([0-9]+)(\s?)(' + item_para_achar + '\s*)|(' + item_para_achar + '\s*)(\s?)(:?)(\s?)([0-9]+)(\s?)']
 	return getItem(soup,item_para_achar,patterns)
 
 def getLocacao(soup,item_para_achar):
@@ -72,25 +75,26 @@ def getLocacao(soup,item_para_achar):
 		retorno = ''
 		patterns = ['((' + item_para_achar + ')\s)(\s?R\$\s?)?(\d+(?:\.\d{3})?)(,?\d{2})?(\s?R\$\s?)?']
 		retorno = getItem(soup,item_para_achar,patterns)
-	return retorno
+	return retorno if itemfound(retorno) else None
 
-url = 'https://www.gpsimoveis.imb.br/imovel/casa-com-4-quartos-para-locacao-r-4-500-00-jardim-portal-dos-ipes-indaiatuba-sp/CA10386-CS0'
-url = 'http://www.visaoimoveisindaiatuba.com.br/alugar/Indaiatuba/Casa/Padrao/Jardim-Sao-Conrado/893402'
-headers = {'User-Agent': generate_user_agent(device_type="desktop", os=('mac', 'linux'))}
-req = requests.get(url,timeout=10,headers=headers)
-if req.status_code == 200:
-	page_content = BeautifulSoup(req.content, 'html5lib')
-	[s.extract() for s in page_content('script')]
-	banheiros = getBanheiros(page_content, '[Bb]anheiros?')
-	quartos = getQuartos(page_content, '[Qq]uartos?')
-	suites = getSuites(page_content, '[Ss]uítes?')
-	locacao = getLocacao(page_content, ['[Aa]luguel','[Ll]ocação'])
-	condominio = acharCondominio(page_content, '[Cc]ondomínio')
-	IPTU = acharCondominio(page_content, 'IPTU')
-	print(banheiros)
-	print(quartos)
-	print(suites)
-	print(locacao)
-	print(condominio)
-	print(IPTU)
-	exit()
+urls = ['https://www.gpsimoveis.imb.br/imovel/casa-com-4-quartos-para-locacao-r-4-500-00-jardim-portal-dos-ipes-indaiatuba-sp/CA10386-CS0','http://www.visaoimoveisindaiatuba.com.br/alugar/Indaiatuba/Casa/Padrao/Jardim-Sao-Conrado/893402']
+imoveis = []
+for url in urls:
+	imovel = {}
+	headers = {'User-Agent': generate_user_agent(device_type="desktop", os=('mac', 'linux'))}
+	req = requests.get(url,timeout=10,headers=headers)
+	if req.status_code == 200:
+		page_content = BeautifulSoup(req.content, 'html5lib')
+		[s.extract() for s in page_content('script')]
+		try:
+			imovel['numero_de_banheiros'] = getBanheiros(page_content, '[Bb]anheiros?')
+			imovel['numero_de_quartos'] = getQuartos(page_content, '[Qq]uartos?')
+			imovel['numero_de_suites'] = getSuites(page_content, '[Ss]uítes?')
+			imovel['valor_de_locacao'] = getLocacao(page_content, ['[Aa]luguel','[Ll]ocação'])
+			imovel['valor_de_condominio'] = acharCondominio(page_content, '[Cc]ondomínio')
+			imovel['valor_de_IPTU'] = acharCondominio(page_content, 'IPTU')
+		except Exception as e:
+			raise e
+		imoveis.append(imovel)
+print(imoveis)
+exit()
